2720deb9b1508d49d1b0767556821fcb7fa16811
Jessica neves <128631606+jessicamdsn@users.noreply.github.com>
2024-07-26 20:21:02 -0300

    fix: problema do setInterval

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
@@ -7,6 +7,48 @@ import { Component } from '@angular/core';
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit, OnDestroy {
+  countdown = {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+  };
 
+  private targetDate: Date = new Date('2025-12-23T00:00:00');
+  private intervalId: any;
+
+  ngOnInit(): void {
+    this.startCountdown();
+  }
+
+  startCountdown() {
+    this.updateCountdown();
+    this.intervalId = setInterval(() => this.updateCountdown(), 1000);
+  }
+
+  updateCountdown() {
+    const now = new Date().getTime();
+    const target = this.targetDate.getTime();
+    const timeDifference = target - now;
+
+    if (timeDifference > 0) {
+      this.countdown.days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      this.countdown.hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.countdown.minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+      this.countdown.seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    } else {
+      this.clearInterval();
+    }
+  }
+
+  clearInterval() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.clearInterval();
+  }
 }
