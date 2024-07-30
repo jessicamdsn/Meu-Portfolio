4dd500386ea5491c082cab9a29b2f8e71578e527
Jessica neves <128631606+jessicamdsn@users.noreply.github.com>
2024-07-30 18:02:59 -0300

    add contato part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,21 @@ import { SkillsComponent } from './components/Contagem/contagem.component';
 import { DivisaoComponent } from './components/divisao/divisao.component';
 import { SobreMimComponent } from './components/sobre-mim/sobre-mim.component';
 import { CarrosselComponent } from "./components/carrossel/carrossel.component";
+import { ContatoComponent } from "./components/contato/contato.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, 
-    PgInicialComponent, 
-    GithubComponent, 
-    SkillsComponent, 
-    DivisaoComponent, 
-    SobreMimComponent, 
-    CarrosselComponent
-  ],
+    RouterOutlet,
+    PgInicialComponent,
+    GithubComponent,
+    SkillsComponent,
+    DivisaoComponent,
+    SobreMimComponent,
+    CarrosselComponent,
+    ContatoComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
